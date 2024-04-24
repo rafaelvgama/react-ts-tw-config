a70dff43072d700c209855e19e1d7e5467c0cd46
Rafael Vogas Gama <rafaelvgama@yahoo.com.br>
2024-04-24 20:13:16 -0300

    Readme and Cleaning project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 const App = () => {
   return (
-    <>
-      <div>
-        <a
-          href='https://vitejs.dev'
-          target='_blank'
-          rel='noreferrer'
-        >
-          Vite
-        </a>
-        <a
-          href='https://react.dev'
-          target='_blank'
-          rel='noreferrer'
-        >
-          React
-        </a>
-      </div>
-      <h1 className='text-red-500'>Vite + React</h1>
-    </>
+    <h1>
+      Vite, React, Typescript, Tailwind CSS, Vitest, Eslint + Prettier, Husky +
+      Lint Staged
+    </h1>
   )
 }
 
