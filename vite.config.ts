d2ab84ec522ea30f0743d682da2c4148f9bb0c14
Rafael Vogas Gama <rafaelvgama@yahoo.com.br>
2024-04-25 00:17:28 -0300

    Installing Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules',
+        '**/*.style.ts',
+        '**/*.config.js',
+        '**/*.config.ts',
+        '**/*.d.ts',
+      ],
+    },
+  },
 })
