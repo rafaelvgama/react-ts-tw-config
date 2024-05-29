60fdf844d797a0cfbcf2096aaacc48e40aa6d668
Rafael Vogas Gama <rafaelvgama@yahoo.com.br>
2024-05-28 21:31:57 -0300

    Typescript Path Mapping

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  /* Vite config */
+  plugins: [react(), tsconfigPaths()],
   server: { port: 3000 },
+
+  /* Vitest config */
   test: {
     globals: true,
     environment: 'jsdom',
